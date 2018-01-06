eecb4f0b5b38ad84a7901f9bec92788e7e0b0c83
meirstudio <34937194+meirstudio@users.noreply.github.com>
2018-01-06 15:49:18 +0500

    1
    1

---
+++ chemi/about.php
@@ -0,0 +1,217 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/main.css">
+
+  <title>XimProm</title>
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 col-md-12 col-lg-11 ml-auto mr-auto">
+        <div class="container-fluid">
+          <div class="top-block">
+            <div class="row">
+              <div class="col-12 col-md-3 ml-auto mr-auto">
+                <div class="logo">
+                  <img src="img/logo.png" alt="">
+                </div>
+              </div>
+              <div class="col-md-6 mr-auto">
+                <p class="h3">ТОО <span>Х</span>им<span>П</span>ром.kz</p>
+                <p><b>Наша компания занимается реализацией и поставками лабораторной и химической химии, ГСО, стандарт-титров, органических растворителей, кислот, спиртов, а также необходимых материалов для лабораторий и пройзводства.</b></p><br>
+                <p class="tel">Тел: 8-707-774-74-71</p>
+              </div>
+              <div class="col-md-3 ml-auto">
+                <div class="callback" style="margin-top: 25px;">
+                  <a href="#" class="ring" style="padding:5px 10px; color: #fff">Заказать звонок</a><br>
+                  <a href="#" style="border-bottom: 2px dashed #fff; color: #fff">Отправить сообщение</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- navbar -->
+        <div class="container-fluid">
+          <div class="nav-block">
+            <div class="row">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-12 col-md-12 col-lg-12 ml-auto mr-auto">
+                    <nav class="navbar navbar-expand-md navbar-light bg-default" style="background-color: #0047b8">
+                      <a class="navbar-brand" href="index.php"><p><span>Х</span>им<span>П</span>ром.kz</p></a>
+                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+
+                      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                          <li class="nav-item">
+                            <a class="nav-link" href="index.php" style="color: #fff">ГЛАВНАЯ <span class="sr-only">(current)</span></a>
+                          </li>
+                          <li class="nav-item active">
+                            <a class="nav-link" href="about.php" style="color: #fff">О КОМПАНИИ</a>
+                          </li>
+                          <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="catalog.php" style="color: #fff;" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              КАТАЛОГ
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="#">Химическая продукция</a>
+                              <a class="dropdown-item" href="#">Лабораторная посуда</a>
+                              <a class="dropdown-item" href="#">Лабораторное оборудование и приборы</a>
+                              <a class="dropdown-item" href="#">Лабораторная мебель</a>
+                              <a class="dropdown-item" href="#">Бумаги и фильтры</a>
+                              <a class="dropdown-item" href="#">Вспомагательное оборудование и материалы</a>
+                              <a class="dropdown-item" href="#">Последний экземпляр - низкая цена</a>
+                              <div class="dropdown-divider"></div>
+                            </div>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="contact.php" style="color: #fff">КОНТАКТЫ</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </nav>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Content -->
+        <div class="container-fluid">
+          <div class="row">
+            <div class="content-block">
+              <div class="container-fluid">
+                <div class="row">
+                  <!-- left-col -->
+                  <div class="col-12 col-md-3">
+                    <div class="leftcol">
+                      <form action="#">
+                        <input type="text" placeholder="Поиск">
+                        <button type="submit" class="btn btn-default"><img src="img/search.png" alt=""></button>
+                      </form>
+                      <p class="h3" style="border-bottom: 2px solid silver">О компании</p>
+                      <p class="h3"><b>Каталог</b></p>
+                      <ul>
+                        <li><a href="#">Химическая продукция</a></li>
+                        <li><a href="#">Лабораторная посуда</a></li>
+                        <li><a href="#">Лабораторное оборудование и приборы</a></li>
+                        <li><a href="#">Лабораторная мебель</a></li>
+                        <li><a href="#">Бумаги и фильтры</a></li>
+                        <li><a href="#">Вспомагательное оборудование и материалы</a></li>
+                        <li><a href="#">Последний экземпляр - низкая цена</a></li>
+                      </ul>
+                    </div>  
+                  </div>
+                  <!-- central-col -->
+                  <div class="col-12 col-md-6">
+                    <div class="comtainer-fluid">
+                      <div class="main-content">
+                        
+                          <div class="row">
+                            <div class="col-12">
+                              <div class="container-fluid">
+                                <div class="about-block">
+                                  <div class="row">
+                                    <div class="col-11 col-md-12 col-lg-12 ml-auto mr-auto">
+                                      <div class="top-text">
+                                        <p class="h3">О компании</p><br>
+                                      </div>
+                                      <div class="pre-text">
+                                        <p style="border-bottom: 2px dotted black;"><b>Краткое описание компании</b></p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde quas saepe fugit, consequatur, eos at aut accusamus esse ducimus et! Quia ducimus tenetur blanditiis natus, quidem vel, reiciendis laudantium ipsa culpa aperiam vitae impedit temporibus cupiditate, quae nulla animi pariatur?</p>
+                                      </div>
+                                      <div class="full-text">
+                                        <p style="border-bottom: 2px dotted black;"><b>Полное описание компании</b></p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, nam beatae quae. Error, necessitatibus officia. Ratione temporibus, veniam culpa magnam tenetur sequi, eum iure nobis fugit consectetur. Explicabo, quasi, blanditiis. A at minima quam aperiam, eligendi nihil quas officia nulla facilis, adipisci, laboriosam deleniti, ducimus eaque. Repudiandae architecto ipsa commodi, rerum nihil velit eum tempore non aspernatur voluptate. Explicabo sequi similique sint, tenetur repudiandae commodi libero tempore ex cum, eum autem neque ab, facilis accusamus non dignissimos voluptate aliquid quia? Dolorum quidem quam accusamus libero necessitatibus aliquam exercitationem earum dignissimos ducimus tempora, quaerat veniam, nam rerum ad unde impedit aperiam reiciendis iusto numquam obcaecati, quisquam. Fuga ullam ad sequi dolore earum aspernatur minus nesciunt vitae velit fugit eius quod adipisci tempora recusandae culpa, odio impedit totam beatae debitis et similique consectetur! Cumque excepturi possimus eligendi inventore odio nam repudiandae suscipit magnam tempora, fugiat quisquam dolorum minima, totam iusto quis illo sed et iste provident voluptatibus autem molestias eius animi! Aliquid veniam aut temporibus dolores nobis minus modi nam tenetur sed accusamus quaerat soluta voluptas illum enim repellendus suscipit magnam rem accusantium, odio facilis ratione labore autem placeat, recusandae! Reiciendis velit sint aliquid dolorum porro tempore magnam recusandae sed molestias iusto.
+                                        <br><br>
+                                        <p style="border-bottom: 2px dotted black;"><b>Наши преимущества</b></p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde quas saepe fugit, consequatur, eos at aut accusamus esse ducimus et! Quia ducimus tenetur blanditiis natus, quidem vel, reiciendis laudantium ipsa culpa aperiam vitae impedit temporibus cupiditate, quae nulla animi pariatur?</p>
+                                        <br>
+                                        <p style="border-bottom: 2px dotted black;"><b>Общее сведения</b></p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde quas saepe fugit, consequatur, eos at aut accusamus esse ducimus et! Quia ducimus tenetur blanditiis natus, quidem vel, reiciendis laudantium ipsa culpa aperiam vitae impedit temporibus cupiditate, quae nulla animi pariatur?</p>
+                                      </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                  
+                  <!-- right-col -->
+                  <div class="col-12 col-md-3">
+                    <div class="rightcol">
+                      <div class="captu">
+                        <img src="img/placeholder_200.png" alt="">
+                        <div class="name">
+                        <p><b>Ximprom</b></p>
+                        <span class="e">ximprom.kz@gmail.com</span>
+                        </div>
+                      </div>
+                      <div class="cont">
+                        <a href="#" style="background-color: #ffc96e;">НАПИСАТЬ НАМ</a><br>
+                        <img src="img/phone.png" alt=""> 
+                        <span class="ph">8707-774-74-71</span>
+                        <p>Наш адрес:  <select name="city" id="city">
+                          <option value="1">г.Уральск, ул.Абулхайр хана, 159 офис 52</option>
+                        </select></p>
+                        <p><b>Время работы:</b> пн-пт 9:00-17:00, <b>без перерыва</b></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- next -->
+        <div class="container-fluid">
+          <div class="footer">
+            <div class="row">
+              <div class="container-fluid">
+                <div class="row">
+                  <!-- left-col -->
+                  <div class="col-12 col-md-6 ml-auto mr-auto">
+                    <p>&copy;Ximprom.kz</p><br>
+                    <p>г.Уральск, ул.Абулхайр хана, 159 офис 52</p><br>
+                    <p>Время работы: пн-пт 9:00-17:00 без перерыва</p>
+                  </div>
+                  <div class="col-12 col-md-6 ml-auto mr-auto">
+                    <div class="social">
+                      <a href="http://" title="Мы в facebook" target="_blank"><img src="img/icon/facebook.png" alt=""></a>
+                      <a href="http://" title="Мы в instagram" target="_blank"><img src="img/icon/instagram.png" alt=""></a>
+                      <a href="http://" title="Мы в twitter" target="_blank"><img src="img/icon/twitter.png" alt=""></a>
+                      <a href="http://" title="Мы вконтакте" target="_blank"><img src="img/icon/vk.png" alt=""></a>
+                    </div>  
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>        
+        <!-- next -->
+        <!-- next -->
+        <!-- next -->
+        <!-- next -->
+      </div>
+    </div>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+</body>
+</html>
